1e92bea215ef4cf8f2f0b57f892910b00accf6d3
Gabriela <tborges.gabriela@gmail.com>
2018-08-21 23:32:06 +0000

    merge k sorted lists brute force

--- leetcode/hard/linked_list/merge_k_sorted_lists.py
+++ leetcode/hard/linked_list/merge_k_sorted_lists.py
@@ -11,64 +11,174 @@
 # Output: 1->1->2->3->4->4->5->6
 
 
-class Solution(object):
+class Node(object):
+    def __init__(self, data):
+        self.val = data
+        self.next = None
+
+
+class LinkList(object):
+
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+
+            curr.next = new_node
+
+
+    def print_list(self):
+        if self.head is None:
+            print "there is nothing to print"
+
+        else:
+            curr = self.head
+            while curr.next:
+                print curr.val
+                curr = curr.next
+
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-    #***riginal solution***
 
-    if len(lists) == 0:
-        return None
-    
-    nums = []
-    
-    #iterating through list, grabbing single linked list, iterate through that link list, and putting it onto my newly created list
-    
-    for ll in lists:
-        #have single link list, will iterate through it
-        curr = ll
-        while curr:
-            nums.append(curr.val)
-            curr = curr.next
-        
-            
-    sorted_nums = sorted(nums)
-    
-    if len(sorted_nums) == 0:
-        return None
-        
-        #making a dummy head
-        dummy = ListNode(nums)
-        #linking first element in sorted_nums to dummynode
-        curr =  ListNode(sorted_nums[0])
-        dummy.next = curr
-        
-        for i in range(len(sorted_nums)):
-            if i == 0:
-                continue
-            else:
-                node = ListNode(sorted_nums[i])
-                curr.next = node
-                curr = node
-                
-        return dummy.next
-
-    #***alternate, cleaner solution***
+        #### BRUTE FORCE SOLUTION ####
         nums = []
 
-        for head in lists:
-            current = head
-            while current:
-                nums.append(current.val)
-                current = current.next
+        for ll in lists:
+            curr = ll.head
 
-        sorted_list = sorted(nums)
-        head = ListNode(0)
-        current = head
-        for num in sorted_list:
-            current.next = ListNode(num)
-            current = current.next
+            while curr:
+                nums.append(curr.val)
+                curr = curr.next
 
-        return head.next
+        nums.sort()
+        dummy = dummy_head = Node(0)
+        for num in nums:
+            # print num
+            dummy.next = Node(num)
+            dummy = dummy.next
+
+
+        return dummy_head.next
+
+
+
+            
+
+
+
+
+if __name__ == '__main__':
+    ll1 = LinkList()
+    ll1.append(1)
+    ll1.append(2)
+    ll1.append(3)
+    ll1.append(4)
+    ll2 = LinkList()
+    ll2.append(5)
+    ll2.append(6)
+    ll2.append(7)
+    ll2.append(8)
+    ll3 = LinkList()
+    ll3.append(9)
+    ll3.append(10)
+    ll3.append(11)
+    ll3.append(12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def mergeKLists(self, lists):
+#         """
+#         :type lists: List[ListNode]
+#         :rtype: ListNode
+#         """
+#     #***riginal solution***
+
+#     if len(lists) == 0:
+#         return None
+    
+#     nums = []
+    
+#     #iterating through list, grabbing single linked list, iterate through that link list, and putting it onto my newly created list
+    
+#     for ll in lists:
+#         #have single link list, will iterate through it
+#         curr = ll
+#         while curr:
+#             nums.append(curr.val)
+#             curr = curr.next
+        
+            
+#     sorted_nums = sorted(nums)
+    
+#     if len(sorted_nums) == 0:
+#         return None
+        
+#         #making a dummy head
+#         dummy = ListNode(nums)
+#         #linking first element in sorted_nums to dummynode
+#         curr =  ListNode(sorted_nums[0])
+#         dummy.next = curr
+        
+#         for i in range(len(sorted_nums)):
+#             if i == 0:
+#                 continue
+#             else:
+#                 node = ListNode(sorted_nums[i])
+#                 curr.next = node
+#                 curr = node
+                
+#         return dummy.next
+
+#     #***alternate, cleaner solution***
+#         nums = []
+
+#         for head in lists:
+#             current = head
+#             while current:
+#                 nums.append(current.val)
+#                 current = current.next
+
+#         sorted_list = sorted(nums)
+#         head = ListNode(0)
+#         current = head
+#         for num in sorted_list:
+#             current.next = ListNode(num)
+#             current = current.next
+
+#         return head.next
