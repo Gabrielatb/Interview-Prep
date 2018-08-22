927929bcaf8e1c936c664585864dbf0b5886e70e
Gabriela <tborges.gabriela@gmail.com>
2018-08-22 22:02:34 +0000

    longest_univalue_path clearner solution

--- leetcode/easy/trees/longest_univalue_path.py
+++ leetcode/easy/trees/longest_univalue_path.py
@@ -28,48 +28,65 @@
 # 2
 # Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
 
-class BinarySearchNode(object):
-    """Binary tree node."""
 
-    def __init__(self, key, left=None, right=None):
-        self.val = key
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-        self.left = left
-        self.right = right
 
-    def __repr__(self):
-        """Debugging-friendly representation."""
 
-        return "<BinaryNode %s>" % self.data
 
-    def longestUnivaluePath(self, root):
-      """
-      :type root: TreeNode
-      :rtype: int
-      """
-      if root.left is None and root.right is None:
-        return 0
-      if root.left: 
-        if root.left.val == root.val:
-          left_subtree = self.longestUnivaluePath(root.left)
-      left_subtree = -1
-      if root.right:
-        if root.right.val == root.val:
-          right_subtree = self.longestUnivaluePath(root.right)
-      right_subtree = -1
-      print right_subtree
-      print left_subtree
-      return max(left_subtree, right_subtree) + 1
-
+#Time Complexity O(n), n is number of nodes in a tree
+#Space Complexity(O(h)) which is height of tree, recursive call stack will be H layers deep
+class Solution(object):
+    def depth_first_search(self, root):
+            if root is None:
+                return 0
+            left = self.depth_first_search(root.left)
+            right = self.depth_first_search(root.right)
         
+            if root.left and root.left.val == root.val:
+                left +=1
+            else:
+                left =0
+            if root.right and root.right.val == root.val:
+                right +=1
+            else:
+                right =0     
+                
+            self.count = max(self.count, left+right)
+            return max(left, right)
+     
+    def longestUnivaluePath(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.count = 0
+        self.depth_first_search(root)
+        return self.count
 
 
 
-if __name__ == '__main__':
-    # Create sample tree and search for nodes in it
-    five_three = BinarySearchNode(1)
-    one_two = BinarySearchNode(1)
-    one = BinarySearchNode(1)
-    five_two = BinarySearchNode(5, None, five_three)
-    four = BinarySearchNode(4, one, one_two)
-    five = BinarySearchNode(5, four, five_two)
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     # Create sample tree and search for nodes in it
+#     five_three = BinarySearchNode(1)
+#     one_two = BinarySearchNode(1)
+#     one = BinarySearchNode(1)
+#     five_two = BinarySearchNode(5, None, five_three)
+#     four = BinarySearchNode(4, one, one_two)
+#     five = BinarySearchNode(5, four, five_two)
