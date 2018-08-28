0c79436ae52a4ee55964c9e5322520ed221c1562
Gabriela <tborges.gabriela@gmail.com>
2018-08-28 00:28:17 +0000

    convert array into bst

--- leetcode/easy/trees/convert_sorted_array _to_BST.py
+++ leetcode/easy/trees/convert_sorted_array _to_BST.py
@@ -1,6 +1,8 @@
-# Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+# Given an array where elements are sorted in ascending order, 
+#convert it to a height balanced BST.
 
-# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+# For this problem, a height-balanced binary tree is defined as a binary tree 
+#in which the depth of the two subtrees of every node never differ by more than 1.
 
 # Example:
 
@@ -14,50 +16,107 @@
 #    /   /
 #  -10  5
 
-class BinarySearchNode(object):
-    """Binary tree node."""
-
-    def __init__(self, key, left=None, right=None):
-        self.val = key
-
+class BT(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
         self.left = left
         self.right = right
 
-    def __repr__(self):
-        """Debugging-friendly representation."""
 
-        return "<BinaryNode %s>" % self.val
+def height_balanced(lst):
+    if len(lst) == 0:
+        return None
 
-    def sortedArrayToBST(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
-        if len(nums) == 0:
-            return None
-        mid = len(nums)/2
-        node = BinarySearchNode(nums[mid])
-        node.left = self.sortedArrayToBST(nums[:mid])
-        node.right = self.sortedArrayToBST(nums[mid + 1:])
-        
-        return node
+    mid_indx = len(lst) / 2
 
+    node = BT(lst[mid_indx]) 
+    node.right = height_balanced(lst[mid_indx+1:])
+    node.left = height_balanced(lst[:mid_indx])
 
-
-
+    print node.data
 
 
 
 
 
 if __name__ == '__main__':
-#     five_three = BinarySearchNode(5)
-#     one_two = BinarySearchNode(3)
-#     one = BinarySearchNode(1)
-#     five_two = BinarySearchNode(5, None, five_three)
-#     four = BinarySearchNode(2, one, one_two)
-#     five = BinarySearchNode(5, four, five_two)
 
-     # three = BinarySearchNode(3)
-     # one = BinarySearchNode(1)
-    # two = BinarySearchNode(2, one, three)
+    print height_balanced([-10,-3,0,5,9])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class BinarySearchNode(object):
+#     """Binary tree node."""
+
+#     def __init__(self, key, left=None, right=None):
+#         self.val = key
+
+#         self.left = left
+#         self.right = right
+
+#     def __repr__(self):
+#         """Debugging-friendly representation."""
+
+#         return "<BinaryNode %s>" % self.val
+
+#     def sortedArrayToBST(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: TreeNode
+#         """
+#         if len(nums) == 0:
+#             return None
+#         mid = len(nums)/2
+#         node = BinarySearchNode(nums[mid])
+#         node.left = self.sortedArrayToBST(nums[:mid])
+#         node.right = self.sortedArrayToBST(nums[mid + 1:])
+        
+#         return node
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+# #     five_three = BinarySearchNode(5)
+# #     one_two = BinarySearchNode(3)
+# #     one = BinarySearchNode(1)
+# #     five_two = BinarySearchNode(5, None, five_three)
+# #     four = BinarySearchNode(2, one, one_two)
+# #     five = BinarySearchNode(5, four, five_two)
+
+#      # three = BinarySearchNode(3)
+#      # one = BinarySearchNode(1)
+#     # two = BinarySearchNode(2, one, three)
--- leetcode/easy/trees/find_height_binary_tree.py
+++ leetcode/easy/trees/find_height_binary_tree.py
@@ -1,36 +1,90 @@
-#find height of binary search tree
-class BinarySearchNode(object):
-    """Binary tree node."""
+            #  5
+            #  /\
+            # 4  6
+            # /    \
+            # 3     7
 
-    def __init__(self, key, left=None, right=None):
-        self.data = key
 
+
+
+
+
+
+class BT(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
         self.left = left
         self.right = right
 
-    def __repr__(self):
-        """Debugging-friendly representation."""
+    def find_height(self):
 
-        return "<BinaryNode %s>" % self.data
-
-    def find_height(self, root):
-        #height of empty tree is -1
-        if root is None:
-            return -1
-        left_height = self.find_height(root.left)
-        print left_height
-        right_height = self.find_height(root.right)
-        print right_height
-
-        return max(left_height, right_height) + 1 
+        
 
 
         
+
+
+
 if __name__ == '__main__':
-# Create sample tree and search for nodes in it
-    five_three = BinarySearchNode(1)
-    one_two = BinarySearchNode(1)
-    one = BinarySearchNode(1)
-    five_two = BinarySearchNode(5, None, five_three)
-    four = BinarySearchNode(4, one, one_two)
-    five = BinarySearchNode(5, four, five_two)
+    three = BT(3)
+    seven = BT(7)
+    six = BT(6, None, seven)
+    four = BT(4, three)
+    five = BT(5, four, six)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#find height of binary search tree
+# class BinarySearchNode(object):
+#     """Binary tree node."""
+
+#     def __init__(self, key, left=None, right=None):
+#         self.data = key
+
+#         self.left = left
+#         self.right = right
+
+#     def __repr__(self):
+#         """Debugging-friendly representation."""
+
+#         return "<BinaryNode %s>" % self.data
+
+#     def find_height(self, root):
+#         #height of empty tree is -1
+#         if root is None:
+#             return -1
+#         left_height = self.find_height(root.left)
+#         print left_height
+#         right_height = self.find_height(root.right)
+#         print right_height
+
+#         return max(left_height, right_height) + 1 
+
+
+        
+# if __name__ == '__main__':
+# # Create sample tree and search for nodes in it
+#     five_three = BinarySearchNode(1)
+#     one_two = BinarySearchNode(1)
+#     one = BinarySearchNode(1)
+#     five_two = BinarySearchNode(5, None, five_three)
+#     four = BinarySearchNode(4, one, one_two)
+#     five = BinarySearchNode(5, four, five_two)
