192f714fd3bb4f42c83c26df009eefa86673287e
Gabriela <tborges.gabriela@gmail.com>
2018-08-29 18:40:28 +0000

    intersection of two link list

--- cracking_the_coding_interview/linked_list/intersection.py
+++ cracking_the_coding_interview/linked_list/intersection.py
@@ -1,75 +1,116 @@
 #problem number 2.7
 
 
-#Also leetcode problem
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Write a program to find the node at which the intersection of two singly linked lists begins.
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_node
+
+        self.tail = new_node
+        return new_node
+
+    def append1(self, data):
+        new_node = data
+        if self.head is None:
+            self.head = new_node
+
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_node
+
+        self.tail = new_node
 
 
-# For example, the following two linked lists:
-
-# A:          a1 → a2
-#                    ↘
-#                      c1 → c2 → c3
-#                    ↗            
-# B:     b1 → b2 → b3
-# begin to intersect at node c1.
+    def print_list(self):
+        if self.head is None:
+            print "no nodes to print"
 
 
-# Notes:
-
-# If the two linked lists have no intersection at all, return null.
-# The linked lists must retain their original structure after the function returns.
-# You may assume there are no cycles anywhere in the entire linked structure.
-# Your code should preferably run in O(n) time and use only O(1) memory.
+        curr = self.head
+        while curr:
+            print curr
+            curr = curr.next
 
 
+def intersection(ll1, ll2):
 
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 
-class Solution(object):
-    def getIntersectionNode(self, headA, headB):
-        """
-        :type head1, head1: ListNode
-        :rtype: ListNode
-        """
-        if headA is None or headB is None:
-            return None
-        
-        len_A = 0
-        len_B = 0
-        currA = headA
-        currB = headB
-        
-        while currA:
-            currA = currA.next
-            len_A +=1
-            
-        while currB:
-            currB = currB.next
-            len_B +=1
-            
-        if len_B > len_A:
-            diff = len_B - len_A
-            for _ in range(diff):
-                headB = headB.next
-                
-        elif len_A > len_B:
-            diff = len_A - len_B
-            for _ in range(diff):
-                headA = headA.next
-            
-                
-                
-        while headA and headB:
-            if headA == headB:
-                return headA
-            else:
-                headA = headA.next
-                headB = headB.next
+    #a1 -> a2  
+    #            ->c1 -> c2 ->c3
+    #b1 -> b2 ->b3
 
-        return None
-        
+
+    length1 = 0
+    length2 = 0
+
+    curr1 = ll1.head
+    curr2 = ll2.head
+
+    while headA:
+        headA = headA.next
+        length1 +=1
+
+
+    while headB:
+        headB = headB.next
+        length1 +=1
+
+    while length1 > length2:
+        curr1 = curr1.next
+        length1 -=1
+
+    while length2 > length1:
+        curr2 = curr2.next
+        length2 -=1
+
+
+    while curr1 and curr2:
+        if curr1 == curr2:
+            return curr1
+        curr1 = curr1.next
+        curr2 = curr2.next
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         
