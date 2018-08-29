1d297ef57b8d29925464d037be008fc6a9047a84
Gabriela <tborges.gabriela@gmail.com>
2018-08-29 00:07:28 +0000

    sum list

--- cracking_the_coding_interview/linked_list/sum_lists.py
+++ cracking_the_coding_interview/linked_list/sum_lists.py
@@ -1,70 +1,92 @@
-#problem also on Leetcode:
+# 7->1->6, 5->9->2
 
-# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+#617 + 295
+#returning 9->1->2
 
-# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-# Example
-
-# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-# Output: 7 -> 0 -> 8
-# Explanation: 342 + 465 = 807.
+class ListNode(object):
+    def __init__(self, data):
+        self.val = data
+        self.next = None
 
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+    def append(self, data):
+        new_node = ListNode(data)
+
+        if self.head is None:
+            self.head = new_node
+
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_node
+        self.tail = new_node
+
+    def print_list(self):
+        if self.head is None:
+            return None
+
+        curr = self.head
+        while curr:
+            print curr.val
+
+    def sum_list(self, ll1, ll2):
         
-        count1 = 0
-        mult1 = 1
-        
-        while l1:
-            if mult1 == 1:
-                count1 += l1.val
-            else:
-                count1 += l1.val * mult1
-            mult1 = mult1 * 10
-            l1 = l1.next    
-        print "count: " + str(count1)
-                
-       
-            
-            
-        count2 = 0
-        mult2 = 1
-        
-        while l2:
-            if mult2 == 1:
-                count2 += l2.val
-            else:
-                count2 += l2.val * mult2
-            mult2 = mult2 * 10
-            l2 = l2.next
-                
-        print "count: " + str(count2)
-                
-            
-            
-        final_count = count1 + count2
-        final_count = str(final_count)
-        
+        counter = 1
+        total = 0
+        count1 = count2 = 0
+        curr1, curr2 = ll1.head, ll2.head
+
+        while curr1:
+
+            count1 += (curr1.val) * counter
+            counter *=10
+            curr1 = curr1.next
+
+        counter = 1    
+        while curr2:
+
+            count2 += (curr2.val) * counter
+            counter *=10
+            curr2 = curr2.next
+
+
+        total = str(count1 + count2)
         prev = None
-        for num in final_count:
-            new_node = ListNode(num)
-            new_node.next = prev
-            prev = new_node
-            
-        return prev
-            
-            
-            
+
+        for num in total:
+            num = int(num)
+            node = ListNode(num)
+            node.next = prev
+            prev = node
+
+        return prev.val
+
+#Time complexity O(max(m, n))
+#Space Complexity O(max(m,n))
+
+
+
+
+if __name__ == '__main__':
+    ll1 = LinkedList()
+    ll1.append(7)
+    ll1.append(1)
+    ll1.append(6)
+
+    ll2 = LinkedList()
+    ll2.append(5)
+    ll2.append(9)
+    ll2.append(2)
+
+
+
+
+
+
+
