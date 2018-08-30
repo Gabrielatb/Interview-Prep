017a9cb02783f07c1749988c755cb674ebe1ee00
Gabriela <tborges.gabriela@gmail.com>
2018-08-30 23:32:06 +0000

    recursive solutino for creating BST from lst

--- cracking_the_coding_interview/trees_graphs/min_tree.py
+++ cracking_the_coding_interview/trees_graphs/min_tree.py
@@ -1,28 +1,87 @@
 #given sorted array with unique integer elements, write an algorithm to create 
 #binary tree
 
-class Node(object):
-    def __init__(self, data):
-        self.data
+
+class BT(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
         self.left = left
         self.right = right
 
 
+def create(lst):
 
-class BST(object):
+#Time Complexity: O(n)
+#Space Complexity O(n)
 
-    def __init__(self, root):
-        self.root = root
+    if len(lst) == 0:
+        return None
+
+    mid = (len(lst) / 2) -1 
+    node = BT(lst[mid])
+
+    node.left = create(lst[:mid])
+    node.right = create(lst[mid+1:])
 
 
-    def min_tree(self, inlist):
-        if len(inlist) == 0):
-            return None
+    return node.data
 
-        mid_point = len(inlist) / 2
-        mid_element = inlist[mid_point]
-        mid_node = Node(mid_element)
+
+#[1,2,3,4,5,6]
+
+
+#       3
+#     /   \
+#     2     5
+#    /    4  6
+# 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Node(object):
+#     def __init__(self, data):
+#         self.data
+#         self.left = left
+#         self.right = right
+
+
+
+# class BST(object):
+
+#     def __init__(self, root):
+#         self.root = root
+
+
+#     def min_tree(self, inlist):
+#         if len(inlist) == 0):
+#             return None
+
+#         mid_point = len(inlist) / 2
+#         mid_element = inlist[mid_point]
+#         mid_node = Node(mid_element)
 
    
-        mid_node.left = self.min_tree(inlist[:mid_point])
-        mid_node.right = self.min_tree(inlist[mid_point + 1:])
+#         mid_node.left = self.min_tree(inlist[:mid_point])
+#         mid_node.right = self.min_tree(inlist[mid_point + 1:])
