4bcee5ce786cc66477e2b81de55d1142386b4117
Gabriela <tborges.gabriela@gmail.com>
2018-08-31 22:43:27 +0000

    successor recursive solution

--- cracking_the_coding_interview/trees_graphs/successor.py
+++ cracking_the_coding_interview/trees_graphs/successor.py
@@ -5,35 +5,46 @@
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def inorderSuccessor(self, root, p):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :rtype: TreeNode
-        """
-        
-        if root is None:
-            return None
-        
-        lst = []
+def inorderSuccessor(self, root, p):
+    
+    if root is None:
+        return None
+    
+    if p.val < root.val:
+        return self.inorderSuccessor(root.left, p) or root
+    
+    return self.inorderSuccessor(root.right,p)
 
-        while root:
-            if root.val > p.val:
-                lst.append(root.val)
-                root = root.left
+
+# class Solution(object):
+#     def inorderSuccessor(self, root, p):
+#         """
+#         :type root: TreeNode
+#         :type p: TreeNode
+#         :rtype: TreeNode
+#         """
+        
+#         if root is None:
+#             return None
+        
+#         lst = []
+
+#         while root:
+#             if root.val > p.val:
+#                 lst.append(root.val)
+#                 root = root.left
                 
-            elif root.val < p.val:
-                root = root.right
-            else:
-                if root.right is None:
-                    if lst == []:
-                        return 
-                    else:
-                        print lst
-                        return lst[-1]
-                else:
-                    root = root.right
-                    while root.left:
-                        root = root.left
-                    return root
+#             elif root.val < p.val:
+#                 root = root.right
+#             else:
+#                 if root.right is None:
+#                     if lst == []:
+#                         return 
+#                     else:
+#                         print lst
+#                         return lst[-1]
+#                 else:
+#                     root = root.right
+#                     while root.left:
+#                         root = root.left
+                    # return root
