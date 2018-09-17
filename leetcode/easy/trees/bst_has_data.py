f4f43b4367940c1f5a45fcc2ef91ba4109f672cd
Gabriela <tborges.gabriela@gmail.com>
2018-09-17 18:07:44 +0000

    bst has data, recursive, iterative

---
+++ leetcode/easy/trees/bst_has_data.py
@@ -0,0 +1,87 @@
+# BST Has Data
+# Given the root of a binary search root node and some data, determine if that
+# data is in the tree.
+      #       (9)
+      #      /  \
+      #    (6)  (12)
+      #   /    /  \
+      # (1)  (10)  (13)
+
+
+
+    # >>> bst_has_data(root, 13)
+    # True
+    # >>> bst_has_data(root, 1)
+    # False
+
+
+class BST(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+#f(13) = True
+#f(10), left = False, right = False
+#f(12), left = False, right = True
+#XXXf(1), left = False, right= False
+#XXXXf(6), left= False, right = False
+# f(9), left=False , right = True
+
+def find_node(root, data):                       
+
+ #space Complexity: O(h)
+ #Time Complexity O(n)   
+    #recursive                                             
+    if root is None:
+        return False
+
+    if root.data == data:
+        return True
+
+    if root.data > data:
+        return find_node(root.left, data)
+
+    else:
+        return find_node(root.right, data)
+
+    #iterative 
+    #space complexity = O(1)
+    # Time Complexity = O(n)
+    
+    curr = root
+    while curr:
+        if curr.data == data:
+            return True
+
+        elif curr.data > data:
+            curr = curr.left
+        else:
+            curr = curr.right
+
+    return False
+
+    
+
+
+
+
+if __name__ == '__main__':
+    one = BST(1)
+    ten = BST(10)
+    thirteen = BST(13)
+    six = BST(6, one)
+    twelve = BST(12, ten, thirteen)
+    nine = BST(9, six, twelve)
+    # print find_node(nine, 9)
+    # print find_node(nine, 6)
+    # print find_node(nine, 1)
+    # print find_node(nine, 12)
+    # print find_node(nine, 10)
+    # print find_node(nine, 13)
+    print find_node(nine, 99)
+    print find_node(nine, 0)
+    print find_node(nine, -1)
+    print find_node(nine, 1.5)
+
+
---
+++ leetcode/medium/sort/largest_num.py
@@ -0,0 +1,51 @@
+#[10,2]
+
+#[0,0,0]
+
+
+# [3,30,34,5,9]
+
+# 95433
+#[9,5,4,3,4,3,0]
+# Output: "9534330"
+
+def largeNum(nums):
+
+ 
+
+#     count = 0
+    # nums.sort()
+    # print nums
+
+
+    # return_lst = [0] * len(lst_nums)
+
+#     for i in range(1, len(return_lst), 3):
+#         return_lst[-i] = lst_nums[0]
+#         lst_nums = lst_nums[1:]
+#     print return_lst
+# # "9534330"
+#     for indx, elem in enumerate(return_lst):
+#         if elem == 0:
+#             return_lst[indx] = lst_nums[-1] 
+#             lst_nums = lst_nums[:-1]
+#     print return_lst
+    # string = ''
+    # for num in return_lst:
+    #     string += str(num)
+
+
+    # return int(string)
+
+
+       
+
+    # print return_lst
+    # return return_lst
+
+
+print largeNum([3,30,34,5,9])
+
+
+
+
