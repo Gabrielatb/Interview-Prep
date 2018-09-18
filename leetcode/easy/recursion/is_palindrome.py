7ebec8e5e67c311de83c1cf4d1ca0b564de5284f
Gabriela <tborges.gabriela@gmail.com>
2018-09-18 18:52:51 +0000

    is palindrome rec and iterative

--- leetcode/easy/recursion/is_palindrome.py
+++ leetcode/easy/recursion/is_palindrome.py
@@ -1,40 +1,104 @@
 #is palindrome recursive solution
 
-    # iterative solution
-def isPalindrome(s):
-    """
-    :type s: str
-    :rtype: bool
-    """
-    count = 0
-    s = ''.join([char for char in s if char.isalpha() or char.isdigit()])
-    print s
-    s = s.lower()
-    print s
-    for i in range(len(s)/2):
-            if s[i] == s[-(i+1)]:
-                count += 1
-    if count == (len(s)/2):
-        return True
-    return False
+#racecar --> True
 
-    # recursive solution
-def isPalindrome(s):
-    def isChar(s):
-        s = ''.join([char for char in s if char.isalpha() or char.isdigit()])
-        s = s.lower()
-        return s
+#f(e) -> return True
+#f(cec) - >return True
+#f(aceca) -> return True
+#f(racecar) -> return True
+
+
+
+#f(us) - >return False
+#f(ouse) -> return False
+#f(house) -> return False
+
+
+
+#house --> False
+
+#recursive solution
+def pal(word):
+
+    if len(word) <= 1:
+        return True
+
+    else:
+
+        return pal(word[1:-1]) and word[0] == word[-1]
+
+
+
+#iterative
+def pal_iterative(word):
+
+    for i in range(len(word)/2):
+        if word[i] != word[-(i+1)]:
+            return False
+    return True
+
+
+print pal_iterative('racecar')
+print pal_iterative('raceca')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # iterative solution
+# def isPalindrome(s):
+#     """
+#     :type s: str
+#     :rtype: bool
+#     """
+#     count = 0
+#     s = ''.join([char for char in s if char.isalpha() or char.isdigit()])
+#     print s
+#     s = s.lower()
+#     print s
+#     for i in range(len(s)/2):
+#             if s[i] == s[-(i+1)]:
+#                 count += 1
+#     if count == (len(s)/2):
+#         return True
+#     return False
+
+#     # recursive solution
+# def isPalindrome(s):
+#     def isChar(s):
+#         s = ''.join([char for char in s if char.isalpha() or char.isdigit()])
+#         s = s.lower()
+#         return s
     
 
 
-    def isPal(s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if len(s) <= 1:
-            return True
-        else:
-            return s[0] == s[-1] and isPal(s[1:-1])
-    return isPal(isChar(s))
+#     def isPal(s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         if len(s) <= 1:
+#             return True
+#         else:
+#             return s[0] == s[-1] and isPal(s[1:-1])
+#     return isPal(isChar(s))
 
