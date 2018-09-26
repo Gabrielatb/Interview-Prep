57d1cfa5fe1562a86dd190231c74d4772a1888ec
Gabriela <tborges.gabriela@gmail.com>
2018-09-26 17:39:32 +0000

    buy sell stock optimal solution

--- elements_of_programming/buy_sell_stock.py
+++ elements_of_programming/buy_sell_stock.py
@@ -20,9 +20,40 @@
 # Output: 0
 # Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
-def max_profit (lst):
 
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    max_profit = None
+    
+    #[7,6(i),4,3,1]
+    # max_proft = -1
+    
+# time complexity O(n**2)
+# space complexity O(1)
+    maxprofit = 0;
+    for i in range(0, len(prices)):
+        for j in range(i+1, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > maxprofit:
+                maxprofit = profit
+    return maxprofit
 
-
-
+############################################
+#Time Complexity O(n)
+#Space Complexity O(1)
+    max_profit = 0
+    min_price = float('inf')
+    
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price 
+    
+            
+            
+    return max_profit
 
