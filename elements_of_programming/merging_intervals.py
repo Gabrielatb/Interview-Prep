18944a7dde861f12d120e4ad6a165f66db3e33f6
Gabriela <tborges.gabriela@gmail.com>
2018-10-09 21:48:31 +0000

    incomplete fraction

--- elements_of_programming/merging_intervals.py
+++ elements_of_programming/merging_intervals.py
@@ -6,18 +6,18 @@
 # result: [[-4,-1], [0,9], [11, 12], [14,17]]
 
 
-def merge_intervalS(lst, add_int):
+# def merge_intervalS(lst, add_int):
 
-    beg = add_int[0]
-    end = add_int[-1]
-    i = 0
+#     beg = add_int[0]
+#     end = add_int[-1]
+#     i = 0
 
-    while i < len(lst):
-        if beg < lst[i][-1]:
-
-
+#     while i < len(lst):
+#         if beg < lst[i][-1]:
 
 
 
 
-print merge_intervalS([[-4,-1], [0,2], [3,6], [7,9], [11, 12], [14,17]], [0,8])
+
+
+# print merge_intervalS([[-4,-1], [0,2], [3,6], [7,9], [11, 12], [14,17]], [0,8])
--- leetcode/company specific problems/Uber/fraction_to_recurring_decimal.py
+++ leetcode/company specific problems/Uber/fraction_to_recurring_decimal.py
@@ -15,4 +15,33 @@
 # Input: numerator = 2, denominator = 3
 # Output: "0.(6)"
 
+def fractionToDecimal(numerator, denominator):
+    """
+    :type numerator: int
+    :type denominator: int
+    :rtype: str
+    """
+# numerator = 1, denominator = 2
+# Output: "0.5"
 
+#Input: numerator = 2, denominator = 1
+# Output: "2"
+   
+    num, remainder = divmod(numerator, denominator)
+    strng = t = '-' if numerator * denominator < 0 else ''
+    fraction = [strng + str(num)]
+    if remainder == 0:
+        return ''.join(fraction)
+    else:
+        fraction.append('.')
+        dict_ = {}
+        while remainder != 0:
+            if remainder in dict_:
+                
+
+
+# print fractionToDecimal(0, -5)
+# print fractionToDecimal(1, 2)
+# print fractionToDecimal(2, 1)
+# print fractionToDecimal(2, 3)
+print fractionToDecimal(50, 8)
