5cd33f9c61882de4607ea538fa7c8be30386e36f
Gabriela <tborges.gabriela@gmail.com>
2018-10-09 23:49:07 +0000

    word break

---
+++ leetcode/hard/list/word_break_II.py
@@ -0,0 +1,64 @@
+# Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+ # add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+
+# Note:
+
+# The same word in the dictionary may be reused multiple times in the segmentation.
+# You may assume the dictionary does not contain duplicate words.
+# Example 1:
+
+# Input:
+# s = "catsanddog"
+# wordDict = ["cat", "cats", "and", "sand", "dog"]
+# Output:
+# [
+#   "cats and dog",
+#   "cat sand dog"
+# ]
+# Example 2:
+
+# Input:
+# s = "pineapplepenapple"
+# wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+# Output:
+# [
+#   "pine apple pen apple",
+#   "pineapple pen apple",
+#   "pine applepen apple"
+# ]
+# Explanation: Note that you are allowed to reuse a dictionary word.
+# Example 3:
+
+# Input:
+# s = "catsandog"
+# wordDict = ["cats", "dog", "sand", "and", "cat"]
+# Output:[]
+
+
+
+def word_break(s, wordDict):
+# s = "pineapplepenapple"
+# wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+
+
+    return_lst = []
+
+    for word in wordDict:
+        if s.starswith(word):
+            i = len(word)
+            temp = word[i:]
+            poss_breaks = word_break(temp, wordDict)
+            if poss_breaks:
+                for poss_break in word_break(temp, wordDict):
+                    return_lst.append(word + ' ' + poss_break)
+            else:
+                return_lst.append(word)
+
+    return return_lst
+
+
+
+
+
+
+
