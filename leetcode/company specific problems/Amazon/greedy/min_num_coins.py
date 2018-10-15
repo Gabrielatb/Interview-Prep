8b91cee59f0cb491b4d5cbb303d7c40a1856f25f
Gabriela <tborges.gabriela@gmail.com>
2018-10-15 19:56:43 +0000

    min num of coins

---
+++ leetcode/company specific problems/Amazon/greedy/min_num_coins.py
@@ -0,0 +1,4 @@
+# Given a value V, if we want to make change for V Rs, and we have infinite supply
+# of each of the denominations in Indian currency, i.e., we have infinite
+# supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, 
+# what is the minimum number of coins and/or notes needed to make the change?
