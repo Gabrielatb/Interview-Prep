021a9aca3a0537d352cb43a301ac11e161b75c75
Gabriela <tborges.gabriela@gmail.com>
2018-10-15 19:26:17 +0000

    activity selection

--- leetcode/company specific problems/Amazon/greedy/activity_selection.py
+++ leetcode/company specific problems/Amazon/greedy/activity_selection.py
@@ -3,30 +3,51 @@
 # You are given n activities with their start and finish times.
 # Select the maximum number of activities that can be performed by a single person,
 # assuming that a person can only work on a single activity at a time.
-def unsorted(s, f):
-    pass
-  
-def activity_selection(s , f ): 
-    f,s = zip(*sorted(zip(f, s)))
- 
-    count = 1
-    i = 0
 
-    for j in range(len(f)):
-        if s[j] >= f[i]:
+
+
+# Time: O(nlogn)
+# Space: O(1)
+
+def max_activities(s, f):
+    f,s = zip(*sorted(zip(f,s)))
+    #tuples
+
+    count = 1
+    indx = 0
+    for start in range(len(s)):
+        if s[start] >= f[indx]:
+            indx = start
             count +=1
-            i=j
     return count
 
 
 
-s = [20, 25, 10]
+
+
+
+s = [20, 12, 10]
 f = [30, 25, 20]
 
 
-# s = [1 , 3 , 0 , 5 , 8 , 5] 
+# s = [1 , 3 , 0 , 5 , 8 , 5]
 # f = [2 , 4 , 6 , 7 , 9 , 9] 
-print activity_selection(s, f)
+print max_activities(s,f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
