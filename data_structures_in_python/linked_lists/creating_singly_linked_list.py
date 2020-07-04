1b35af63e4deccc6371975f77d242a7d0edb1c5a
gabrielatb <tborges.gabriela@gmail.com>
2020-07-04 13:25:14 -0700

    Singly Linked List deletion

--- data_structures_in_python/linked_lists/creating_singly_linked_list.py
+++ data_structures_in_python/linked_lists/creating_singly_linked_list.py
@@ -6,66 +6,52 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    #O(1)
+    # O(1)
     def get_head(self):
         return self.head
     # O(1)
     def is_empty(self):
-        if self.head == None:
-            return True
-        return False
+        if self.head:
+            return False
+        return True
     # O(1)
     def insert_at_head(self, data):
+        print("inserting at head", data)
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
-        return self.head
     # O(n)
     def print_lst(self):
-        if(self.is_empty()):
-            print("List is Empty")
+        if (self.is_empty()):
+            print("List is Empty!")
             return False
-        node = self.head
-        while node.next:
-            print(node.data, end=" -> ")
-            node = node.next
-        print(node.data, "-> None")
+        curr = self.head
+        while curr.next:
+            print(curr.data, end="=>")
+            curr = curr.next
+        print(curr.data, end="=>None")
+        print("")
         return True
     # O(n)
     def insert_at_tail(self, data):
         new_node = Node(data)
-        if self.is_empty():
+        if (self.is_empty()):
             self.head = new_node
             return
-        curr = self.head 
+        curr = self.head
         while curr.next:
-            print(curr.data, end=" -> ")
             curr = curr.next
         curr.next = new_node
-        print(new_node.data, "-> None")
-
-    def insert_at_k(self, data, k):
-        # new_node = Node(data)
-        # if self.is_empty():
-        #     self.head = new_node
-        #     return
-        # curr = self.head 
-        # while curr.next:
-        #     print(curr.data, end=" -> ")
-        #     curr = curr.next
-        # curr.next = new_node
-        # print(new_node.data, "-> None")
-
-
 
 
 
 link_list = LinkedList()
-# print(link_list.get_head())
-# print(link_list.is_empty())
+print(link_list.get_head())
+print(link_list.is_empty())
 
 print("Inserting values in list")
-for i in range(1, 10):
+for i in range(1, 11):
     link_list.insert_at_head(i)
-# link_list.print_lst()
+link_list.print_lst()
 link_list.insert_at_tail(200)
+link_list.print_lst()
--- data_structures_in_python/linked_lists/list_deletion.py
+++ data_structures_in_python/linked_lists/list_deletion.py
@@ -7,61 +7,73 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert(self, value):
-        new_node = Node(value)
-        if self.head == None:
-            self.head = new_node
-        else:
-            curr = self.head
-            while curr.next:
-                curr = curr.next
-            curr.next = new_node
+    def insert_at_head(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
+    def is_empty(self):
+        if self.head:
+            return False
+        return True
 
-    def print_list(self):
-        if self.head == None:
-            print("Empty List!")
-        else:
-            curr = self.head
-            while curr.next:
-                print(curr.data, end="->")
-                curr = curr.next
-            print(curr.data, end="->None")
+    def print_lst(self):
+        if self.is_empty():
+            print ("List is Empty")
+            return False
 
-    # O(1)
+        curr = self.head 
+
+        while curr.next:
+            print (curr.data, end="->")
+            curr = curr.next
+        print(curr.data, end="->None")
+        print("")
+        return True
+
     def delete_at_head(self):
-        if self.head == None:
-            print("Empty List!")
-        else:
-            head_to_delete = self.head
-            self.head  = self.head.next
-            head_to_delete.next = None
+        print ("inside delete at head")
+        if self.is_empty():
+            return False
 
-    # O(N)
-    # def delete_at_value(self, value):
-    #     if self.head == None:
-    #         print("Empty List!")
-    #         return False
-    #     else:
-    #         curr = self.head
-    #         prev = None
-    #         while curr:
-    #             if curr.data == value:
-    #                 curr.next = prev
-    #                 return True
-    #             prev = curr
-    #             curr = curr.next
-    #         return False
+        curr = self.head
+        self.head = self.head.next
+        curr.next = None
+        return True
+
+    # O(n)
+    def delete_at_value(self, value):
+        if self.is_empty():
+            return False
+
+        if self.head.data == value:
+            self.delete_at_head()
+            return True
+
+        prev = None
+        curr = self.head
+        while curr.next:
+            if curr.data == value:
+                prev.next = curr.next
+                curr.next = None
+                return True
+            prev = curr
+            curr = curr.next
+
+        if curr.data == value:
+            prev.next = curr.next
+            curr.next = None
+            return True
+        return False
 
 
-# 1 -> 2 -> 3 -> 4 -> 5 -> None
+
 
 ll = LinkedList()
-ll.insert(1)
-ll.insert(2)
-ll.insert(3)
-ll.insert(4)
-ll.insert(5)
-# ll.delete_at_head()
-print(ll.delete_at_value(4))
-ll.print_list()
+
+for num in range(1, 11)[::-1]:
+    ll.insert_at_head(num)
+
+ll.print_lst()
+ll.delete_at_value(10)
+ll.print_lst()
